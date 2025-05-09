4f69f5cd424a1fc7acbd602ad01bfe08e037aeb2
Marwin Fischer <marwin.fischer@gmx.de>
2025-05-09 08:28:35 +0200

    Add predict_offence_pos

--- algo/prediction.py
+++ algo/prediction.py
@@ -2,6 +2,24 @@ import copy
 
 from algo.model import Entity, Field, Vec
 
+def predict_offence_pos(entity: Entity, field: Field, x_pos_offence) -> tuple[Vec, Vec]:
+    """Predict the position and velocity of the puck for a given x coordinate.
+    Raises ValueError if the velocity in x direction is zero.
+    """
+    if entity.v.x == 0:
+        raise ValueError("Velocity in x direction is zero, cannot predict position.")
+
+    # Calculate the time it would take to reach the given x coordinate
+    time_to_reach_x = (x_pos_offence - entity.pos.x) / entity.v.x
+
+    # Calculate the new position and velocity after that time
+    pos, v = predict(entity, field, time_to_reach_x)
+
+    # Set the x coordinate to the given value
+    pos.x = x_pos_offence
+
+    return pos, v
+
 
 def predict(entity: Entity, field: Field, dt: float) -> tuple[Vec, Vec]:
     min_border = min(field.w, field.h)
